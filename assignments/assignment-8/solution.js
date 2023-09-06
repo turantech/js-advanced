24c6caca5c359c0bcd00b5d2f4842c8461b71c42
Ali Ramazon <mr.safarali@gmail.com>
2023-09-06 11:14:52 -0600

    Provide solution for assignment-8

--- assignments/assignment-8/solution.js
+++ assignments/assignment-8/solution.js
@@ -9,53 +9,41 @@ const processArray = (arr, callback) => {
 // 2. Closure with Increment Function
 const counter = () => {
     let count = 0;
-    const increment = () => {
-        count = count + 1;
-        return count;
-    };
+    const increment = () => ++count;
     return increment;
 };
 
 // 3. Function Using a Helper Function with Loop
 const sumOfSquares = (numbers) => {
-    const square = (x) => {
-        return x * x;
-    };
+    const square = (x) => x * x;
     let sum = 0;
-    for (let i = 0; i < numbers.length; i++) {
-        sum = sum + square(numbers[i]);
+    for (let number of numbers) {
+        sum += square(number);
     }
     return sum;
 };
 
 // 4. Function with Multiple Helper Functions
 const mathOperations = (a, b) => {
-    const add = () => {
-        return a + b;
-    };
-    const multiply = () => {
-        return a * b;
-    };
-    return { add: add, multiply: multiply };
+    const add = () => a + b;
+    const multiply = () => a * b;
+    return { add, multiply };
 };
 
 // 5. Function with Closure and Callback
-const createMultiplier = (multiplier) => {
-    return (number) => {
-        return number * multiplier;
-    };
-};
+const createMultiplier = (multiplier) => (number) => number * multiplier;
 
 // 6. Function with Multiple Callbacks
-const stringManipulation = (str, operation1, operation2) => {
-    return operation2(operation1(str));
-};
+const stringManipulation = (str, operation1, operation2) =>
+    operation2(operation1(str));
 
 // 7. Function with Callback and Loop
 const computeAverage = (nums, callback) => {
     let sum = 0;
     for (let i = 0; i < nums.length; i++) {
-        sum = sum + nums[i];
+        sum += nums[i];
     }
     return callback(sum / nums.length);
 };
+
+// Summary: All functions have been converted into arrow functions.
